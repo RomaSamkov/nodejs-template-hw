3c3a7d372f8b49e683051b0dba6409c09fb367bb
RomaSamkov <Romansamkov.rs@gmail.com>
2022-09-28 18:01:57 +0300

    delete files

--- models/products/updateProducts.js
+++
@@ -1,8 +0,0 @@
-const filePath = require('./filePath');
-const fs = require('fs/promises');
-
-const updateProducts = async products => {
-  await fs.writeFile(filePath, JSON.stringify(products));
-};
-
-module.exports = updateProducts;
