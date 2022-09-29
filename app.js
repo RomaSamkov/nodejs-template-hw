e34c3fe7cbd501b600a10619227bc772b6b9cfea
RomaSamkov <Romansamkov.rs@gmail.com>
2022-09-29 17:13:23 +0300

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const cors = require('cors')
 require("dotenv").config()
 
 const booksRouter = require("./routes/api/books")
-const contactsRouter = require('./routes/api/contacts')
 
 const app = express()
 
@@ -15,7 +14,6 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/api/books', booksRouter)
-app.use('/api/contacts', contactsRouter)
 
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found' })
